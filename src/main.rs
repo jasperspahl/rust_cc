53f8c47ea47f96163fc63bc26a082db38a284458
Jasper Spahl <jasperspahl@web.de>
2023-01-23 21:39:40 +0100

    some changes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use nannou::{color, prelude::*};
-use rand::seq::SliceRandom;
+use nannou::prelude::*;
+use std::cmp::min;
 
 const WIN_W: u32 = 600;
 const WIN_H: u32 = 900;
@@ -7,7 +7,14 @@ const SQUARE_W: f32 = 60.0;
 const SQUARE_H: f32 = 80.0;
 const NUM_X: i32 = 7;
 const NUM_Y: i32 = 9;
-const MAX_NUM_SQUARES: usize = 10;
+const MAX_NUM_SQUARES: usize = 8;
+const COLORS: &[&[f32; 3]; 5] = &[
+    &[0.24, 0.24, 0.24], // 0.72
+    &[0.1, 0.43, 0.65],  // 1.18
+    &[0.38, 0.68, 0.67], // 1.73
+    &[0.92, 0.54, 0.38], // 1,84
+    &[0.65, 0.54, 0.68], // 1,87
+];
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -18,16 +25,19 @@ struct Square {
     weight: f32,
 }
 impl Square {
-    pub fn new() -> Self {
-        let w = SQUARE_W;
-        let h = SQUARE_H;
+    pub fn new(randomness: f32, i: usize) -> Self {
+        let w = SQUARE_W / 2.2;
+        let h = SQUARE_H / 2.2;
+
+        let min = 0.99 - randomness * (i as f32) * 0.2;
+        let max = 1.0 + randomness * (i as f32) * 0.2;
 
         let mut points = vec![];
-        let point1 = pt2(random_f32() * -w, random_f32() * -h);
+        let point1 = pt2(random_range(min, max) * -w, random_range(min, max) * -h);
         points.push(point1);
-        points.push(pt2(random_f32() * w, random_f32() * -h));
-        points.push(pt2(random_f32() * w, random_f32() * h));
-        points.push(pt2(random_f32() * -w, random_f32() * h));
+        points.push(pt2(random_range(min, max) * w, random_range(min, max) * -h));
+        points.push(pt2(random_range(min, max) * w, random_range(min, max) * h));
+        points.push(pt2(random_range(min, max) * -w, random_range(min, max) * h));
         points.push(point1);
 
         let weight = random_range::<f32>(1.0, 3.0);
@@ -38,7 +48,7 @@ impl Square {
 struct SquareStructure {
     position: Vec2,
     collection: Vec<Square>,
-    color: color::Rgb,
+    randomness: f32,
 }
 
 impl SquareStructure {
@@ -53,37 +63,25 @@ impl SquareStructure {
         let x = x - (max_x * 0.3);
         let y = y - (max_y * 0.35);
 
+        let randomness = (i_y - (i_x + 1.0)) / 8.0;
+        println!("x = {}, y = {}, randomness = {}", i_x, i_y, randomness);
+
         let collection = (0..MAX_NUM_SQUARES)
             .into_iter()
-            .map(|_| Square::new())
+            .map(|i| Square::new(randomness, i))
             .collect();
 
         SquareStructure {
             position: pt2(x, y),
             collection,
-            color: SquareStructure::rand_color(),
+            randomness,
         }
     }
 
-    pub fn rand_color() -> color::Rgb {
-        let colors = vec![
-            rgb(0.38, 0.68, 0.67),
-            rgb(0.1, 0.43, 0.65),
-            rgb(0.65, 0.54, 0.68),
-            rgb(0.92, 0.54, 0.38),
-            rgb(0.24, 0.24, 0.24),
-        ];
-        let color = colors.choose(&mut rand::thread_rng()).unwrap();
-        *color
-    }
-
-    pub fn set_color(&mut self) {
-        self.color = Self::rand_color();
-    }
     pub fn set_points(&mut self) {
         self.collection = (0..MAX_NUM_SQUARES)
             .into_iter()
-            .map(|_| Square::new())
+            .map(|i| Square::new(self.randomness, i))
             .collect();
     }
 }
@@ -103,6 +101,7 @@ struct Model {
     _window: WindowId,
     squares: Vec<SquareStructure>,
     current_num_squares: usize,
+    export: usize,
 }
 
 fn model(app: &App) -> Model {
@@ -120,11 +119,23 @@ fn model(app: &App) -> Model {
     Model {
         _window,
         squares,
-        current_num_squares: MAX_NUM_SQUARES,
+        current_num_squares: 1,
+        export: 0,
     }
 }
 
-fn mouse_released(_app: &App, model: &mut Model, button: MouseButton) {
+fn save_frame(app: &App, model: &mut Model) {
+    let file_path = app
+        .project_path()
+        .expect("failed to locate `project_path`")
+        .join(app.exe_name().unwrap())
+        .join(format!("{:03}", model.export))
+        .with_extension("png");
+    app.main_window().capture_frame(file_path);
+    model.export +=1;
+}
+
+fn mouse_released(app: &App, model: &mut Model, button: MouseButton) {
     match button {
         MouseButton::Left => {
             for structure in model.squares.iter_mut() {
@@ -132,24 +143,24 @@ fn mouse_released(_app: &App, model: &mut Model, button: MouseButton) {
             }
         }
         MouseButton::Right => {
-            for structure in model.squares.iter_mut() {
-                structure.set_color();
-            }
+            save_frame(app, model);
         }
         _ => {}
     }
 }
 
-fn mouse_wheel(_app: &App, model: &mut Model, dt: MouseScrollDelta, _phase: TouchPhase) {
+fn mouse_wheel(app: &App, model: &mut Model, dt: MouseScrollDelta, _phase: TouchPhase) {
     match dt {
         MouseScrollDelta::LineDelta(_, y) if y > 0.0 => {
             if model.current_num_squares < MAX_NUM_SQUARES {
                 model.current_num_squares += 1;
+                save_frame(app, model);
             }
         }
         MouseScrollDelta::LineDelta(_, y) if y < 0.0 => {
             if model.current_num_squares > 1 {
                 model.current_num_squares -= 1;
+                save_frame(app, model);
             }
         }
         _ => {}
@@ -163,10 +174,15 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.background().rgb(0.88, 0.87, 0.85);
 
     for structure in model.squares.iter() {
-        for square in structure.collection.iter().take(model.current_num_squares) {
+        for (i, square) in structure.collection.iter().enumerate().take(min(
+            model.current_num_squares,
+            (structure.randomness * 8.0).abs() as usize + 1,
+        )) {
+            let cc = COLORS[i % COLORS.len()];
+            let c = rgb(cc[0], cc[1], cc[2]);
             draw.polyline()
                 .xy(structure.position)
-                .color(structure.color)
+                .color(c)
                 .weight(square.weight)
                 .points(square.points.clone());
         }
